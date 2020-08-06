1cea9e2bb8e7b531e4a4826b61e646e1d51f0c53
Breno Novelli <bnojpa@gmail.com>
2020-08-06 18:14:30 -0300

    Test movie page 25%

---
+++ src/__tests__/pages/Movie.spec.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { act, render } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
+import MockAdapter from 'axios-mock-adapter';
+
+import api from '../../services/api';
+import movieResponse from '../../__mocks__/movie.data.json';
+import movieNotFoundResponse from '../../__mocks__/movie-not-found.data.json';
+import Movie from '../../pages/Movie';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    id: 'tt123456',
+  }),
+}));
+
+jest.mock('../../hooks/favourites', () => {
+  return {
+    useFavourites: () => ({
+      favoriteMovie: jest.fn(),
+      favourites: [],
+      favouritesCount: 20,
+    }),
+  };
+});
+
+const apiMock = new MockAdapter(api);
+
+describe('Movie Page', () => {
+  it('should be able to render the movie', async () => {
+    const helmetContext = {};
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <HelmetProvider context={helmetContext}>
+          <Movie />
+        </HelmetProvider>
+      </MemoryRouter>,
+    );
+
+    expect(getByText(/loading/i)).toBeTruthy();
+
+    await act(async () => {
+      await apiMock.onGet('&i=tt123456').reply(200, { ...movieResponse });
+    });
+
+    expect(getByText('Casa de mi Padre')).toBeTruthy();
+  });
+
+  it('should be able to show error message on incorrect IMDb ID', async () => {
+    const helmetContext = {};
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <HelmetProvider context={helmetContext}>
+          <Movie />
+        </HelmetProvider>
+      </MemoryRouter>,
+    );
+
+    expect(getByText(/loading/i)).toBeTruthy();
+
+    await act(async () => {
+      await apiMock
+        .onGet('&i=tt123456')
+        .reply(200, { ...movieNotFoundResponse });
+    });
+
+    expect(getByText('Incorrect IMDb ID.')).toBeTruthy();
+  });
+});
